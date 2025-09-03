8110173a106a25aab671ad907d34b4ae6075aef3
Charlie Heuer <charlie_heuer@asl.org>
2025-09-03 13:56:46 +0100

    fixed Student.java

--- src/main/java/com/csase/PlannerRunner.java
+++ src/main/java/com/csase/PlannerRunner.java
@@ -4,6 +4,7 @@ public class PlannerRunner {
     public static void main(String[] args) {
 
         // Create a new student
+        
         Student jada = new Student();
 
         // Gets the student's choice until the student exits
